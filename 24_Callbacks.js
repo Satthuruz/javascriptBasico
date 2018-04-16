1b641d67f8a86bf49ea0fa60b1d45873d4b51c2b
Stalin <satthuruz@gmail.com>
2018-04-16 10:45:34 -0500

    Agregando proceso al callback

--- 24_Callbacks.js
+++ 24_Callbacks.js
@@ -1,8 +1,12 @@
 setTimeout(function callback(){
     console.log('Mensaje A')
-},300)
+},1000)
+//
+for(let i = 0; i < 9999999999; i ++){};
 
 setTimeout(function callback(){
     console.log('Mensaje B')
 },0)
+
+console.log('Mensaje C')
 
